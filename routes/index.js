3708570cff9ab724b991384dd929f9cb54c82274
Tam T Dang <teedang19@gmail.com>
2014-06-27 14:42:49 -0500

    populates db with users if none

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,48 @@
 var User = require('../models/user');
 
+function createUsers() {
+  console.log('createUsers has been called');
+
+  var userObjs = [
+    { "username": "tamdang", "fullName": "Tam Dang", "admin": false },
+    { "username": "paigecrum", "fullName": "Paige Crum", "admin": false },
+    { "username": "clarkkampfe", "fullName": "Clark Kampfe", "admin": false },
+    { "username": "fraugoop", "fullName": "Frau Goop the Ruthless Cat", "admin": false },
+    { "username": "kevinsolorio", "fullName": "Kevin Solorio", "admin": false },
+    { "username": "paulyshore", "fullName": "Pauly Shore", "admin": true}
+  ];
+
+  userObjs.forEach(function(userAttributes){
+    var newUser = new User(userAttributes);
+
+    newUser.save(function(err, user){
+      if(err){
+        console.log(err);
+        console.log("error in saving new users");
+      } else {
+        console.log("success in creating new users");
+      }
+    });
+
+  });
+};
+
 module.exports = function(app) {
 
   app.get('/', function(req, res) {
 
-    User.find(function(err, allUsers) {
+    User.find({}, function(err, allUsers) {
       if (err) {
         // please god implement some error handling
-        conosle.log("ERROR ERROR HALP")
+        console.log("ERROR ERROR HALP")
         console.log(err)
         return;
       } else {
-        // check if allUsers is empty
         if(allUsers.length == 0) {
-          // if so, populate database
-          console.log("length is zero");
+          createUsers();
         } else {
-        // if not, uhm, continue ...
           console.log("length is not zero")
         }
-        
         console.log("no errors");
         res.render('index', { title: 'Chicago Bakery Hunt' });
       }
@@ -27,16 +50,19 @@ module.exports = function(app) {
 
   });
 
+  app.get('/admin/users', function(req, res){
+    User.find({}, function(err, allUsers) {
+      if (err) {
+        return "serious error handling needed soon";
+      } else{
+        res.json(allUsers);
+      }
+    })
+  });
+
+  app.post('/users', function(req, res){
+    // console.log(req);
+    // console.log("XXXXXXXXX");
+  });
+
 }
-
-// GIVEN, reconfigured.
-
-// var express = require('express');
-// var router = express.Router();
-
-/* GET home page. */
-// app.get('/', function(req, res) {
-//   res.render('index', { title: 'Chicago Bakery Hunt' });
-// });
-
-// module.exports = router;
