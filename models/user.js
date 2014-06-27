c77a1d8caf4650ab8bd04e7a92c1419243c023d5
Tam T Dang <teedang19@gmail.com>
2014-06-27 12:22:14 -0500

    user mongoose model, schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,15 @@
 var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username: String,
+  password: String,
+  fullName: String,
+  admin: Boolean,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
