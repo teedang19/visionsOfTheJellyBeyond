535ea9b7496aa875365534396e040e5d3aeae4dc
Tam T Dang <teedang19@gmail.com>
2014-06-27 12:26:19 -0500

    bakery model cleared accidentally review model

--- models/bakery.js
+++ models/bakery.js
@@ -1,15 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var reviewSchema = new Schema({
-  _userId: String,
-  _bakeryId: String,
-  foodReview: String,
-  placeReview: String,
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+var bakerySchema = new Schema({
+
 });
 
-var Review = new mongoose.model('Review', reviewSchema);
+var Bakery = new mongoose.model('Bakery', bakerySchema);
 
-module.exports = Review;
+module.exports = Bakery;
