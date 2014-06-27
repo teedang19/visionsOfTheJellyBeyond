2227a960658e841aee0aabe24c214ed293d1c1b6
Tam T Dang <teedang19@gmail.com>
2014-06-27 12:25:37 -0500

    review mongoose model and schema

--- models/review.js
+++ models/review.js
@@ -1,1 +1,15 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var reviewSchema = new Schema({
+  _userId: String,
+  _bakeryId: String,
+  foodReview: String,
+  placeReview: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+var Review = new mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
