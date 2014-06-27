fe8133e6eb1e15421363f7dc172e22a10e9e36e3
Tam T Dang <teedang19@gmail.com>
2014-06-27 12:30:36 -0500

    bakery mongoose model and schema OK

--- models/bakery.js
+++ models/bakery.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var bakerySchema = new Schema({
-
+  name: String,
+  address: String,
+  website: String,
+  contactName: String,
+  description: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
 var Bakery = new mongoose.model('Bakery', bakerySchema);
